4d2576c32920250bd6feb86ce7619d879b896a20
yiskaneto <44711170+yiskaneto@users.noreply.github.com>
2024-03-18 17:18:39 -0600

    main vara and outputs updated

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,9 @@
-output "full_name" {
-  description = "A string of the form 'orgname/reponame'."
-  value       = github_repository.flux_github_repo.full_name
+output "id" {
+  description = "(String) The ID of this resource."
+  value       = flux_bootstrap_git.this.id
 }
 
-output "html_url" {
-  description = "URL to the repository on the web."
-  value       = github_repository.flux_github_repo.html_url
-}
-
-output "ssh_clone_url" {
-  description = "URL that can be provided to git clone to clone the repository via SSH."
-  value       = github_repository.flux_github_repo.ssh_clone_url
-}
-
-output "http_clone_url" {
-  description = "URL that can be provided to git clone to clone the repository via HTTPS."
-  value       = github_repository.flux_github_repo.http_clone_url
-}
-
-output "git_clone_url" {
-  description = "URL that can be provided to git clone to clone the repository anonymously via the git protocol."
-  value       = github_repository.flux_github_repo.git_clone_url
-}
-
-output "repo_id" {
-  description = "GitHub ID for the repository"
-  value       = github_repository.flux_github_repo.repo_id
+output "repository_files" {
+  description = "(Map of String) Git repository files created and managed by the provider."
+  value       = flux_bootstrap_git.this.repository_files
 }
--- variables.tf
+++ variables.tf
@@ -1,38 +1,70 @@
-variable "name" {
+variable "path" {
   type        = string
-  description = "(Required) The name of the repository."
+  description = "(String) Path relative to the repository root, when specified the cluster sync will be scoped to this path."
 }
 
-variable "description" {
-  type        = string
-  description = "(Optional) A description of the repository."
+variable "cluster_domain" {
+  type = string
+  description = "(String) The internal cluster domain. Defaults to cluster.local"
+  default = "cluster.local"
 }
 
-variable "visibility" {
-  type        = string
-  description = "(Optional) A description of the repository."
+variable "components" {
+  type = set(string)
+  description = "(Set of String) Toolkit components to include in the install manifests. Defaults to [source-controller kustomize-controller helm-controller notification-controller]"
+  default = ["source-controller", "kustomize-controller", "helm-controller", "notification-controller"]
 }
 
-variable "delete_branch_on_merge" {
-  type        = bool
-  description = "(Optional) Automatically delete head branch after a pull request is merged. Defaults to false."
-  default     = false
+variable "components_extra" {
+  type      = set(string)
+  description = "List of extra components to include in the install manifests."
+  default = [ "image-reflector-controller", "image-automation-controller" ]
 }
 
-variable "auto_init" {
-  type        = bool
-  description = "(Optional) Set to true to produce an initial commit in the repository."
-  default     = false
+variable "interval" {
+  type = string
+  description = "(String) Interval at which to reconcile from bootstrap repository. Defaults to 1m0s."
+  default = "1m0s"
 }
 
-variable "homepage_url" {
-  type        = string
-  description = "(Optional) URL of a page describing the project."
-  default     = null
+variable "log_level" {
+  type = string
+  description = "(String) Log level for toolkit components. Valid values are info, debug, error. /Defaults to info."
+  default = "info"
 }
 
-variable "topics" {
-  type        = list(string)
-  description = "(Optional) The list of topics of the repository."
-  default     = []
+variable "manifests_path" {
+  type = string
+  description = "(String) The install manifests are built from a GitHub release or kustomize overlay if using a local path. Defaults to https://github.com/fluxcd/flux2/releases."
+  default = "https://github.com/fluxcd/flux2/releases"
+}
+
+variable "namespace" {
+  type = string
+  description = "(String) The namespace scope for install manifests. Defaults to flux-system. It will be created if it does not exist."
+  default = "flux-system"
+}
+
+variable "network_policy" {
+  type = bool
+  description = "(Boolean) Deny ingress access to the toolkit controllers from other namespaces using network policies. Defaults to true."
+  default = true
+}
+
+variable "secret_name" {
+  type = string
+  description = "(String) Name of the secret the sync credentials can be found in or stored to. Defaults to flux-system."
+  default = "flux-system"
+}
+
+variable "flux_version" {
+  type = string
+  description = "(String) Flux version. Defaults to v2.2.3"
+  default = "v2.2.3"
+}
+
+variable "watch_all_namespaces" {
+  type = bool
+  description = "(Boolean) If true watch for custom resources in all namespaces. Defaults to true."
+  default = true
 }
